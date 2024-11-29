c50755cfd98fc212e2099e5cccb4c5ae4afecbb9
AbhishekJupalli-1409 <jupalliabhishek1409@gmail.com>
2024-11-29 19:56:14 +0530

    added search bar

---
+++ src/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {
+        placeholderColor: ['responsive', 'focus', 'hover'],
+      },
+    },
+    plugins: [],
+  };
+  
